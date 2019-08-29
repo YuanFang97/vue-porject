aa0005189557157a71391eb6a76044cba0cc89df
chenwei <1755561497@qq.com>
2019-08-29 08:55:12 +0800

    Text2

--- vue-porject/src/components/Special.vue
+++ vue-porject/src/components/Special.vue
@@ -1,10 +1,14 @@
 <template>
     <div>
         主题
+        <div>      
+            <img src="http://s1.hgcang.com/bsimg/dede_uploads/allimg/170925/1-1F9251039130-L.jpg" class="spe_t" alt="">
+        </div>
     </div>
 </template>
 
 <script>
+
 export default {
     
 }
